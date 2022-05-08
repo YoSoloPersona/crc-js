86e7316afe3990da227ff646a0f2e7842631bbbf
YoSoloPersona <79009569+YoSoloPersona@users.noreply.github.com>
2022-05-08 13:38:30 +0300

    Added data types: string and number[]

--- readme.md
+++ readme.md
@@ -13,10 +13,14 @@ npm install yo-crc-js
 import { crc16IBM, crc16IBMi, crc32IEEE, crc32IEEEi } from '../index';
 
 // string '123456789'
-const data = new Uint8Array([ 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 ]);
+const uint8Array = new Uint8Array([ 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 ]);
+const arrayNumber = [ 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 ];
+const str = '123456789';
 
-const crc = crc16IBM(data);
 
-console.log(crc);
-
+console.log(`
+    crc16IBM (data: Uint8Array) => ${crc16IBM(uint8Array)},
+    crc16IBM (data: number[]) => ${crc16IBM(arrayNumber)},
+    crc16IBM (data: string) => ${crc16IBM(str)}
+    `);
 ```
--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,22 +1,62 @@
 import { crc16IBM, crc16IBMi, crc32IEEE, crc32IEEEi } from '../index';
 
 // string '123456789'
-const data = new Uint8Array([ 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 ]);
+const uint8Array = new Uint8Array([ 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 ]);
+const str = '123456789';
+const arrayNumber = [ 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 ];
 
 describe('#index', () => {
-    it('crc16IBM', () => {
-        expect(crc16IBM(data)).toBe(BigInt(0xBB3D));
+    describe('calculate with data type Uint8Array', () => {
+        it('crc16IBM', () => {
+            expect(crc16IBM(uint8Array)).toBe(BigInt(0xBB3D));
+        });
+    
+        it('crc16IBMi', () => {
+            expect(crc16IBMi(uint8Array)).toBe(BigInt(0xB4C8));
+        });
+    
+        it('crc32IEEE', () => {
+            expect(crc32IEEE(uint8Array)).toBe(BigInt(0x2DFD2D88));
+        });
+    
+        it('crc32IEEEi', () => {
+            expect(crc32IEEEi(uint8Array)).toBe(BigInt(0xCBF43926));
+        });
     });
 
-    it('crc16IBMi', () => {
-        expect(crc16IBMi(data)).toBe(BigInt(0xB4C8));
+    describe('calculate with data type string', () => {
+        it('crc16IBM', () => {
+            expect(crc16IBM(str)).toBe(BigInt(0xBB3D));
+        });
+    
+        it('crc16IBMi', () => {
+            expect(crc16IBMi(str)).toBe(BigInt(0xB4C8));
+        });
+    
+        it('crc32IEEE', () => {
+            expect(crc32IEEE(str)).toBe(BigInt(0x2DFD2D88));
+        });
+    
+        it('crc32IEEEi', () => {
+            expect(crc32IEEEi(str)).toBe(BigInt(0xCBF43926));
+        });
     });
-
-    it('crc32IEEE', () => {
-        expect(crc32IEEE(data)).toBe(BigInt(0x2DFD2D88));
-    });
-
-    it('crc32IEEEi', () => {
-        expect(crc32IEEEi(data)).toBe(BigInt(0xCBF43926));
+    
+    describe('calculate with data type number[]', () => {
+        it('crc16IBM', () => {
+            expect(crc16IBM(arrayNumber)).toBe(BigInt(0xBB3D));
+        });
+    
+        it('crc16IBMi', () => {
+            expect(crc16IBMi(arrayNumber)).toBe(BigInt(0xB4C8));
+        });
+    
+        it('crc32IEEE', () => {
+            expect(crc32IEEE(arrayNumber)).toBe(BigInt(0x2DFD2D88));
+        });
+    
+        it('crc32IEEEi', () => {
+            expect(crc32IEEEi(arrayNumber)).toBe(BigInt(0xCBF43926));
+        });
     });
 });
